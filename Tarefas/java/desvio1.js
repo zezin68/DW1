4a3ec9479e50ced133f4f3ab7f4d22c18d6937d6
Eduardo <cheligaedu@gmail.com>
2024-09-08 10:18:29 -0300

    1

---
+++ Tarefas/java/desvio1.js
@@ -0,0 +1,21 @@
+function desvio1(cond1, cond2, cond3) {
+    console.log("1");
+    console.log("2");
+
+    if (cond1) {
+        if (cond3) {
+            console.log("A");
+        } else {
+            console.log("B");
+        }
+    } else {
+        if (cond2) {
+            console.log("C");
+        } else {
+            console.log("D");
+        }
+    }
+
+    console.log("3");
+    console.log("4");
+}
